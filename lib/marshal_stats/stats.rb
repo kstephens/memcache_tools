1c96ca8cec7295bda6a976d50f8a57fc28c73434
Kurt Stephens <kurt@enova.com>
2012-07-26 19:41:20 -0500

    Use Terminal::Table.

--- lib/marshal_stats/stats.rb
+++ lib/marshal_stats/stats.rb
@@ -1,4 +1,6 @@
 require 'marshal_stats'
+gem 'terminal-table'
+require 'terminal-table'
 
 class MarshalStats
 class Stats
@@ -179,7 +181,7 @@ class Stats
       else
         bar = "*" * x
       end
-      bar = "|#{bar}#{' ' * (@width - bar.size)}|"
+      bar = "#{bar}#{' ' * (@width - bar.size)}"
       bar
     end
 
@@ -194,7 +196,8 @@ class Stats
 
   class Histogram
     attr_accessor :values
-    attr_accessor :width, :height
+    attr_accessor :min, :max
+    attr_accessor :width, :height, :show_sum
 
     def initialize values = nil
       @values = values
@@ -203,15 +206,18 @@ class Stats
     end
 
     def generate
+      raise TypeError, "@values not set" unless @values
       return [ ] if @values.size < 2
       @x_graph = Graph.new(@values, @width)
       return [ ] if @x_graph.empty?
       @x_graph.fix_width!
 
-      @buckets = Hash.new { |h, k| h[k] = Bucket.new }
+      @buckets = Hash.new { |h, k| b = Bucket.new; b.name = k; h[k] = b }
       @values.each do | v |
         i = @x_graph.v_to_x(v).to_i
-        @buckets[i].add! v
+        if i >= 0 and i < @x_graph.width
+          @buckets[i].add! v
+        end
       end
 
       cnt = @buckets.values.map { |b| b.count }
@@ -220,89 +226,93 @@ class Stats
       return [ ] if @cnt_graph.empty?
       # @cnt_graph.fix_width!
 
+      if @show_sum
       sum = @buckets.values.map { |b| b.sum }
       sum << 0
       @sum_graph = Graph.new(sum, @height)
       # @sum_graph.fix_width!
+      end
 
       # binding.pry
 
       rows = [ ]
+      table =
+        Terminal::Table.new() do | t |
 
-      # Header
-      row = [ ]
-      row << '%30s' % ''
-      row << '-'
-      row << '%30s' % ''
-      row << '%30s' % 'cnt'
-      row << ('=' * (@cnt_graph.width + 2))
-      row << '%30s' % 'min'
-      row << '%30s' % 'avg'
-      row << '%30s' % 'max'
-      row << '%30s' % 'sum'
-      row << ('=' * (@sum_graph.width + 2))
-      rows << row
+        s = t.style
+        s.border_x =
+          s.border_y =
+          s.border_i = ''
+        s.padding_left = 0
+        s.padding_right = 1
 
-      @width.times do | i |
-        x0 = @x_graph.x_to_v(i).to_i
-        x1 = @x_graph.x_to_v(i + 1).to_i
-        b = @buckets[i]
-        b.finish!
-
-        row = [ ]
-        row << x0
-        row << '-'
-        row << x1
-        row << b.count
-        row << @cnt_graph.bar(b.count)
-        row << b.min
-        row << b.avg
-        row << b.max
-        row << b.sum
-        row << @sum_graph.bar(b.sum || 0)
-        rows << row
-      end
-
-      rows.each do | r |
-        ci = -1
-        r.map! do | c |
-          ci += 1
-          h_size = rows[0][ci].size
-          case c
-          when nil
-            c = (' ' * (h_size - 1)) << '-'
-          when Numeric
-            c = '%*d' % [ h_size, c.to_i ]
-          end
-          c
+        # Header:
+        h = [ '<', '>', 'cnt', '%', "cnt h", "min", "avg", "max" ]
+        align_right = [ 0, 1, 2, 3, 5, 6, 7 ]
+        if @show_sum
+          h.push('sum', '%', 'sum h')
+          align_right.push(8, 9)
         end
-      end
+        rows << h
 
-      # binding.pry
+        cnt_sum = sum_sum = 0
+        @width.times do | i |
+          x0 = @x_graph.x_to_v(i).to_i
+          x1 = @x_graph.x_to_v(i + 1).to_i - 1
+          b = @buckets[i]
+          b.finish!
 
-      leading_ws = [ ]
-      rows.each do | r |
-        r.each_with_index do | c, ci |
-          ws = 0
-          if c =~ /^(\s+)/
-            ws = $1.size
+          cnt_sum += b.count
+          r = [ ]
+          r << x0
+          r << x1
+          r << b.count
+          r << @cnt_graph.percent(b.count)
+          r << @cnt_graph.bar(b.count)
+          r << b.min
+          r << (b.avg && (@cnt_graph.values_are_integers ? b.avg.to_i : b.avg))
+          r << b.max
+          if @show_sum
+            sum_sum += b.sum || 0
+            r << b.sum
+            r << @sum_graph.percent(b.sum || 0)
+            r << @sum_graph.bar(b.sum || 0)
           end
-          leading_ws[ci] ||= ws
-          leading_ws[ci] = ws if leading_ws[ci] > ws
+          rows << r
         end
+
+        f = [ '', '=', cnt_sum, '', '', '', '', '' ]
+        if @show_sum
+          f.push(sum_sum, '', '')
+        end
+        rows << f
+
+        rows.each do | r |
+          r.map! do | c |
+            case c
+            when nil
+              ''
+            when Integer
+              thousands(c)
+            else
+              c
+            end
+          end
+          t << r
+        end
+
+        raise unless h.size == f.size
+
+        align_right.each { | c | t.align_column(c, :right) }
       end
 
-      # binding.pry
-      formatted=
-      rows.map do | r |
-        ci = -1
-        r.map do | c |
-          c[leading_ws[ci += 1] .. -1]
-        end * ' '
-      end
-      # binding.pry
+      formatted = table.to_s.split("\n")
 
       formatted
+    end
+
+    def thousands x
+      x && x.to_s.reverse!.gsub(/(\d{3})/, "\\1,").reverse!.sub(/^(\D|\A),/, '')
     end
   end
 
